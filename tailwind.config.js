2847172b38f4cfd954550d68c19779ad039639af
ulascan54 <ulascandemirbag@gmail.com>
2021-09-18 17:01:00 +0300

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode:'jit',
+  purge: ['./src/assets/style.css'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
