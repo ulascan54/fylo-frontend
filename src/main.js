03e615442c6a266adcbf6367986b4194ccac403d
ulascan54 <ulascandemirbag@gmail.com>
2021-09-18 17:38:04 +0300

    main components created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/style.css'
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/style.css";
+import appButton from "../src/components/ui/subs/appButton.vue";
+import appDetail from "../src/components/ui/subs/appDetail.vue";
+import appLink from "../src/components/ui/subs/appLink.vue";
+import appLinkBlue from "../src/components/ui/subs/appLinkBlue.vue";
+import appLogo from "../src/components/ui/subs/appLogo.vue";
+import appTitle from "../src/components/ui/subs/appTitle.vue";
+
+const app = createApp(App);
+
+app.component("appButton", appButton);
+app.component("appDetail", appDetail);
+app.component("appLink", appLink);
+app.component("appLinkBlue", appLinkBlue);
+app.component("appLogo", appLogo);
+app.component("appTitle", appTitle);
+app.mount("#app");
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./src/assets/style.css'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      'b-email': 'hsl(217, 28%, 15%)',
+      'b-mail': 'hsl(218, 28%, 13%)',
+      'b-footer': 'hsl(216, 53%, 9%)',
+      'b-testimonials':'hsl(219, 30%, 18%)',
+      
+      'g-call-cyan': 'hsl(176, 68%, 64%)',
+      'g-call-blue': 'hsl(198, 60%, 50%)',
+      'light-error': 'hsl(0, 100%, 63%)'
+      }
+    },
   },
   variants: {
     extend: {},
