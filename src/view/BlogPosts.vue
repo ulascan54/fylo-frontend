9359bdec81f643691bc3d65cf0ebd063f6b77144
ulascan54 <ulascandemirbag@gmail.com>
2021-10-18 23:53:21 +0300

    blog page links added

--- src/view/BlogPosts.vue
+++ src/view/BlogPosts.vue
@@ -2,7 +2,15 @@
         <Header />
         <div class="pt-32 pb-20">
             <div class="max-w-[1300px] mx-auto">
-                <h1 class="text-white pt-20 text-3xl font-bold ">Blog Posts</h1>
+                <h1 class="text-white pt-20 text-4xl font-bold ">Blog Posts</h1>
+                <div class="border mt-5"></div>
+                <div class="w-1/2 flex flex-wrap justify-between">
+                <Link :text="'All'" link="#Features"/>
+                <Link :text="'Teclonogies'" link="#Features"/>
+                <Link :text="'Historical'" link="#Features"/>
+                <Link :text="'Psycological'" link="#Team"/>
+                <Link :text="'Matmathic'" link="#"/>
+                </div>
             </div>
             <Blog />
         </div>
