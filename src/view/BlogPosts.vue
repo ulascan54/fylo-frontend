5607ecb07643d8df47965e4324db2d1fbf2a2a6c
ulascan54 <ulascandemirbag@gmail.com>
2021-10-25 23:23:19 +0300

    the links of the blogpost add style for links

--- src/view/BlogPosts.vue
+++ src/view/BlogPosts.vue
@@ -4,12 +4,12 @@
             <div class="max-w-[1300px] mx-auto">
                 <h1 class="text-white pt-20 text-4xl font-bold ">Blog Posts</h1>
                 <div class="border mt-5"></div>
-                <div class="w-1/2 flex flex-wrap justify-between">
-                <Link :text="'All'" link="#Features"/>
-                <Link :text="'Teclonogies'" link="#Features"/>
-                <Link :text="'Historical'" link="#Features"/>
-                <Link :text="'Psycological'" link="#Team"/>
-                <Link :text="'Matmathic'" link="#"/>
+                <div class="w-1/2 flex flex-wrap justify-between mt-2">
+                <Link :text="'All'" link="#Features"  class="text-2xl opacity-100 bg-white rounded-xl px-3 text-b-testimonials underline"/>
+                <Link :text="'Teclonogies'" link="#Features" class="text-2xl  px-3"/>
+                <Link :text="'Historical'" link="#Features"  class="text-2xl  px-3"/>
+                <Link :text="'Psycological'" link="#Team"  class="text-2xl  px-3"/>
+                <Link :text="'Matmathic'" link="#"  class="text-2xl  px-3"/>
                 </div>
             </div>
             <Blog />
